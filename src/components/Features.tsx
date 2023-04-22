459016d438bf9ee91cc54d45a214b3b789938d62
Ngoc Ngo <ngocnv371@gmail.com>
2023-04-22 12:30:53 +0700

    add icons

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -28,10 +28,10 @@ const Features = () => {
               <div key={feature.name} className="relative">
                 <dt>
                   <div
-                    className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-primary border-4`}
+                    className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary`}
                   >
                     <img
-                      className={`inline-block h-6 w-6 rounded-full`}
+                      className={`inline-block rounded-full`}
                       src={feature.icon}
                       alt={feature.name}
                     />
