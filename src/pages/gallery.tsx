d82a8aa580e54386ff708b1ae616ec52f89061bf
Ngoc Ngo <ngocnv371@gmail.com>
2023-06-21 10:36:04 +0700

    add empty header

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 import { ImageList } from '../components/gallery/community-gallery';
+import config from '../config/index.json';
 
 const GalleryPage = () => {
-  return <ImageList />;
+  const { company } = config;
+  const { logo, name: companyName } = company;
+
+  return (
+    <div className={`bg-background grid gap-y-8 overflow-hidden`}>
+      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <nav
+          className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+          aria-label="Global"
+        >
+          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+            <div className="flex items-center justify-between w-full md:w-auto">
+              <Link href="/">
+                <span className="link cursor-pointer">
+                  <span className="sr-only">{companyName}</span>
+                  <img alt="logo" className="h-16 w-auto sm:h-16" src={logo} />
+                </span>
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <ImageList />
+    </div>
+  );
 };
 
 export default GalleryPage;
