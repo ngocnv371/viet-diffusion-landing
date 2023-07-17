7ed3c11e82ce623a8830be7b826dcf18446eb565
Ngoc Ngo <ngocnv371@gmail.com>
2023-07-17 11:08:58 +0700

    update placeholder

--- src/components/gallery/image-skeleton.tsx
+++ src/components/gallery/image-skeleton.tsx
@@ -5,7 +5,7 @@ export const ImageSkeleton: FC<HTMLProps<HTMLImageElement>> = ({
 }) => {
   return (
     <img
-      src="/assets/images/logo-64.png"
+      src="/assets/images/placeholder.png"
       width={384}
       height={576}
       className={`animate-pulse ${className ?? ''}`}
